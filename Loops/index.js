7ff8d51906427b66561b888f233d54211199ce37
philippine2 <49458817+philippine2@users.noreply.github.com>
2021-07-10 11:14:21 -0700

    Add files via upload

---
+++ Loops/index.js
@@ -0,0 +1,23 @@
+/*
+Print out all odd numbers from 1 to 20
+
+
+for(var num=1; num<=20; num++ ){
+    console.log(num)
+}
+
+*/ 
+
+/*
+
+Sum numbers from 1 to 5, printing out the current number and sum so far at each step of the way
+
+var sum = 0
+
+for(var num =1; num<=5 ; num++){
+
+   sum = num+ sum
+
+ console.log( "Num: ", num , "Sum:" ,sum )
+    }
+    */
