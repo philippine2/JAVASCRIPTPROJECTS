35968e3028085967b63f64e2bfb8506db340117f
Your Name <you@example.com>
2021-07-19 22:14:31 -0700

    new update

---
+++ JAVASCRIPTSCourse/index.js
@@ -0,0 +1,44 @@
+console.log("hello")
+
+
+//alert("yoo")
+
+
+//Variable
+var b ='smoothie';
+//console.log(b);
+
+var someNumber= 45;
+//console.log(someNumber);
+
+
+//var age = prompt("What is your age?");
+//document.getElementById('someText').innerHTML = age;
+
+//Numbers in Javascript
+
+// how to increment 
+var num1 = 10;
+num1++
+//num1= num1+1
+console.log(num1);
+
+// how to decrement 
+
+//num1=num1-1
+num1--
+console.log(num1)
+
+// increment or decrement by any number by 10 for example
+
+num1 +=10;
+console.log(num1);
+
+
+
+// divide, multiply,*, remiander %
+
+console.log(num1 % 5 );
+
+
+
