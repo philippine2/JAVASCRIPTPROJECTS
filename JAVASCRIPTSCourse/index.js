84c85edccdd2e6594b3fbc07fb77e26532dc696a
Your Name <you@example.com>
2021-07-19 22:39:54 -0700

    new update

--- JAVASCRIPTSCourse/index.js
+++ JAVASCRIPTSCourse/index.js
@@ -40,5 +40,49 @@ console.log(num1);
 
 console.log(num1 % 5 );
 
+/*function
+1. create a function
+2. call a function
+*/
+// Create
+function fun(){
+    console.log("this is a fucntion");
+}
+//call
+fun();
+
+/* let create a function that take in a name
+and says hello followed by your name*/
+
+var name = 'philia';
+function Name(){
+    console.log("hello"+' '+ name);
+
+}
+//Name();
+function greeting(yourName){
+
+var result = 'hello' + ' ' + yourName;
+console.log(result);
+}
+
+var name = prompt('What is your name?');
+//greeting(name);
+
+
+// How do arguments work in functions?
+// How do we add two numbers together in a function
+function sumNumbers(num1, num2){
+    var result= num1 + num2;
+    console.log(result)
+}
+//sumNumbers(10,10);
+
+
+
+
+
+
+
 
 
